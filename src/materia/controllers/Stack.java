af9bc056e9d24ff49b618bb4008b730271493cef
Einar671 <einarkaalhus2809@gmail.com>
2025-06-03 09:33:32 -0500

    first commit

---
+++ src/materia/controllers/Stack.java
@@ -0,0 +1,48 @@
+package materia.controllers;
+
+import java.util.EmptyStackException;
+
+import materia.models.Node;
+
+public class Stack {
+    
+    private Node top;  
+
+    public Stack(){
+        this.top = null;
+    }
+
+    public void push(int getValue){
+        Node newNode = new Node(getValue);
+        newNode.setNext(top);
+        top = newNode;
+    }
+
+    public int pop(){
+        if(isEmpty()) throw new EmptyStackException();
+        int value = top.getValue();
+        top = top.getNext();
+        return value;
+
+    }
+
+    public boolean isEmpty(){
+        return top == null;
+    }
+
+    public int peak(){
+        if(isEmpty()) throw new EmptyStackException();
+        return top.getValue();
+    }
+
+    public void printStack(){
+        Node actual = top; 
+        System.out.println("---- STACK ----");
+         while (actual != null) {
+            System.out.println("|   " + actual.getValue() + "   |");
+            System.out.println("-----------");
+            actual = actual.getNext();
+        }
+    }
+
+}
