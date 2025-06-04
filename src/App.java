2c8be70c2f379b34473fba3e34fa9cb654649861
Einar671 <einarkaalhus2809@gmail.com>
2025-06-04 18:52:27 -0500

    deber realizado

--- src/App.java
+++ src/App.java
@@ -54,18 +54,57 @@ public class App {
         stack.printSizeOn();
         stack.printStack();*/
 
-        Persona p1 = new Persona("Pablo");
-        Persona p2 = new Persona("Juan");
-        Persona p3 = new Persona("Maria");
+    Persona p1 = new Persona("Pablo");
+    Persona p2 = new Persona("Juan");
+    Persona p3 = new Persona("Maria");
+    Persona p4 = new Persona("Carlos");
 
-        Cola<Persona> colaPersonas = new Cola<Persona>();
-        colaPersonas.add(p1);
-        colaPersonas.add(p2);
-        colaPersonas.add(p3);
-        System.out.println("Tamaño: "+colaPersonas.size());
-        colaPersonas.printQueue();
-        System.out.println("Atiende a la persona => "+ colaPersonas.remove());
-        System.out.println("Tamaño: "+colaPersonas.size());
-        colaPersonas.printQueue();
+    Cola<Persona> colaPersonas = new Cola<Persona>();
+    colaPersonas.add(p1);
+    colaPersonas.add(p2);
+    colaPersonas.add(p3);
+    colaPersonas.add(p4);
+    
+    System.out.println("Cola inicial:");
+    System.out.println("Tamaño: " + colaPersonas.size());
+    colaPersonas.printQueue();
+    
+    System.out.println("\n--- BÚSQUEDA DE PERSONAS ---");
+    Persona encontrada = colaPersonas.buscarPersona("Juan");
+    if (encontrada != null) {
+        System.out.println("Persona encontrada: " + encontrada);
+    } else {
+        System.out.println("Persona no encontrada");
     }
+    
+    System.out.println("¿Existe Maria? " + colaPersonas.existePersona("Maria"));
+    System.out.println("¿Existe Pedro? " + colaPersonas.existePersona("Pedro"));
+    
+    int posicion = colaPersonas.obtenerPosicionPersona("Carlos");
+    System.out.println("Carlos está en la posición: " + posicion);
+    
+    System.out.println("\n--- ELIMINACIÓN DE PERSONAS ---");
+    boolean eliminado = colaPersonas.eliminarPersona("Juan");
+    if (eliminado) {
+        System.out.println("Juan fue eliminado de la cola");
+    } else {
+        System.out.println("Juan no fue encontrado");
+    }
+    
+    System.out.println("\nCola después de eliminar a Juan:");
+    System.out.println("Tamaño: " + colaPersonas.size());
+    colaPersonas.printQueue();
+    
+    boolean eliminado2 = colaPersonas.eliminarPersona("Pedro");
+    System.out.println("¿Se eliminó Pedro? " + eliminado2);
+    
+    boolean eliminado3 = colaPersonas.eliminarPersona("Pablo");
+    System.out.println("¿Se eliminó Pablo? " + eliminado3);
+    
+    System.out.println("\nCola final:");
+    System.out.println("Tamaño: " + colaPersonas.size());
+    colaPersonas.printQueue();
+
+    }
+   
 }
--- src/materia/controllers/Cola.java
+++ src/materia/controllers/Cola.java
@@ -4,6 +4,7 @@ import java.util.EmptyStackException;
 
 import materia.models.Node;
 import materia.models.NodeGeneric;
+import materia.models.Persona;
 
 public class Cola<T> {
     
@@ -59,4 +60,96 @@ public class Cola<T> {
             actuNode = actuNode.getNext();
         }
     }
+
+    /**
+ * Busca una persona por nombre en la cola
+ * @param nombre El nombre de la persona a buscar
+ * @return La persona encontrada o null si no existe
+ */
+public T buscarPersona(String nombre) {
+    if (isEmpty()) return null;
+    
+    NodeGeneric<T> actual = primero;
+    while (actual != null) {
+        T valor = actual.getValue();
+        // Verificamos si es una instancia de Persona y si el nombre coincide
+        if (valor instanceof Persona) {
+            Persona persona = (Persona) valor;
+            if (persona.getNombre().equalsIgnoreCase(nombre)) {
+                return valor;
+            }
+        }
+        actual = actual.getNext();
+    }
+    return null; // No encontrado
+}
+
+/**
+ * Elimina una persona específica de la cola por nombre
+ * @param nombre El nombre de la persona a eliminar
+ * @return true si se eliminó, false si no se encontró
+ */
+public boolean eliminarPersona(String nombre) {
+    if (isEmpty()) return false;
+    
+    if (primero.getValue() instanceof Persona) {
+        Persona personaPrimero = (Persona) primero.getValue();
+        if (personaPrimero.getNombre().equalsIgnoreCase(nombre)) {
+            primero = primero.getNext();
+            if (primero == null) { // Si era el único elemento
+                ultimo = null;
+            }
+            size--;
+            return true;
+        }
+    }
+    
+    NodeGeneric<T> actual = primero;
+    while (actual.getNext() != null) {
+        T valorSiguiente = actual.getNext().getValue();
+        if (valorSiguiente instanceof Persona) {
+            Persona persona = (Persona) valorSiguiente;
+            if (persona.getNombre().equalsIgnoreCase(nombre)) {
+                NodeGeneric<T> nodoAEliminar = actual.getNext();
+                actual.setNext(nodoAEliminar.getNext());
+                
+                if (nodoAEliminar == ultimo) {
+                    ultimo = actual;
+                }
+                
+                size--;
+                return true;
+            }
+        }
+        actual = actual.getNext();
+    }
+    
+    return false;
+}
+
+
+public boolean existePersona(String nombre) {
+    return buscarPersona(nombre) != null;
+}
+
+public int obtenerPosicionPersona(String nombre) {
+    if (isEmpty()) return -1;
+    
+    NodeGeneric<T> actual = primero;
+    int posicion = 0;
+    
+    while (actual != null) {
+        T valor = actual.getValue();
+        if (valor instanceof Persona) {
+            Persona persona = (Persona) valor;
+            if (persona.getNombre().equalsIgnoreCase(nombre)) {
+                return posicion;
+            }
+        }
+        actual = actual.getNext();
+        posicion++;
+    }
+    
+    return -1;
+}
 }
