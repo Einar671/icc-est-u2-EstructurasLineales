e901b105243b4d7fdbe4ea5169394e7be641cc20
Einar671 <einarkaalhus2809@gmail.com>
2025-06-05 08:53:10 -0500

    Finalizacion ejercicios

--- src/Ejercicio_01_sign/SignValidator.java
+++ src/Ejercicio_01_sign/SignValidator.java
@@ -1,9 +1,39 @@
 package Ejercicio_01_sign;
 
+import materia.controllers.StackG;
 
 public class SignValidator {
-    public boolean isValid(String s){
+
+    public boolean isValid(String s) {
+        StackG<Character> stack = new StackG<>();
         
-        return false;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            }
+            else if (c == ')' || c == '}' || c == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                
+                char lastOpen = stack.pop();
+                
+                if (!isMatchingPair(lastOpen, c)) {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
     }
+    
+    
+    private boolean isMatchingPair(char open, char close) {
+        return (open == '(' && close == ')') ||
+               (open == '{' && close == '}') ||
+               (open == '[' && close == ']');
+    } 
+    
 }
--- src/Ejercicio_02_sign/StackSorter.java
+++ src/Ejercicio_02_sign/StackSorter.java
@@ -4,7 +4,17 @@ import materia.controllers.Stack;
 
 public class StackSorter {
     public void stackSorter(Stack stack){
-
+        Stack auxStack = new Stack();
         
+        while(!stack.isEmpty()){
+            int temp = stack.pop();
+            while (!auxStack.isEmpty() && auxStack.peak() > temp) {
+                stack.push(auxStack.pop());
+            }
+            auxStack.push(temp);
+        }
+        while (!auxStack.isEmpty()){
+            stack.push(auxStack.pop());
+        }
     }
 }
